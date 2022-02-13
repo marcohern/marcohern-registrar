2ed1c6e4109679373e4e6d81730e07336a71bb30
Marco Hernandez <marcohern@gmail.com>
2022-02-13 16:41:28 -0500

    initial files

---
+++ src/RegistrarServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Marcohern\Registrar;
+
+use Illuminate\Support\ServiceProvider;
+
+class RegistrarServiceProvider extends ServiceProvider {
+  public function boot() {
+  }
+
+  public function register() {
+    
+  }
+}
