061ec9cb533f25270da9413a7195667090508f48
Marco Hernandez <marcohern@gmail.com>
2022-02-13 17:08:30 -0500

    Unit testing now works

---
+++ tests/Unit/UnitTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Marcohern\Registrar\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class UnitTest extends TestCase {
+
+  public function test_first() {
+    $this->assertTrue(true);
+  }
+}
